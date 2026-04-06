a44db92fd0ae36e39995e61cf263a6aa4d6c822d
andreeaai80 <andreeaneagu80@yahoo.com>
2026-04-06 14:52:27 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+res.send("Server works 🚀");
+});
+app.post("/ask", async (req, res) => {
+try {
+const userMessage = req.body.message;
+
+const response = await axios.post(
+"https://api.openai.com/v1/chat/completions",
+{
+model: "gpt-4o-mini",
+messages: [{ role: "user", content: userMessage }]
+},
+{
+headers: {
+Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+"Content-Type": "application/json"
+}
+}
+);
+
+res.json({
+reply: response.data.choices[0].message.content
+});
+
+} catch (error) {
+console.log(error);
+res.status(500).json({ error: "Error" });
+}
+});
+
+app.listen(3000, "0.0.0.0", () => {
+console.log("Server running on port 3000");
+});
